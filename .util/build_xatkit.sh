e25230a4bcb5179f856010580376b2863c441004
Gwendal <gwendal.daniel@gmail.com>
2019-10-01 16:22:47 +0200

    add parent and metamodels builds in build.sh

--- .util/build_xatkit.sh
+++ .util/build_xatkit.sh
@@ -7,8 +7,22 @@ e() {
 
 main() {
 	
+	e "Building Xatkit Parent"
+    cd /tmp
+    git clone https://github.com/xatkit-bot-platform/xatkit-releases.git > /dev/null
+    cd xatkit-releases
+    mvn install -DskipTests > /dev/null
+    e "Done"
+
+    e "Building Xatkit Metamodels"
+    cd /tmp
+    git clone https://github.com/xatkit-bot-platform/xatkit-metamodels.git > /dev/null
+    cd xatkit-metamodels
+    mvn install -DskipTests > /dev/null
+    e "Done"
+
 	# Do not print the build log, it is already available in the Xatkit build
-    e "Building Xatkit"
+    e "Building Xatkit Runtime"
     cd /tmp
     git clone https://github.com/xatkit-bot-platform/xatkit.git > /dev/null
     cd xatkit
